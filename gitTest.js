95e9933bd0d8e250199ea2341aee34ed986ceab6
Yi Hu <veronicayi@msn.com>
2021-09-22 15:13:34 +0200

    add gitTest

---
+++ gitTest.js
@@ -0,0 +1,8 @@
+function sayHi() {
+    const message = document.getElementById("message")
+    message.innerHTML = "hi" //comment
+    const message1 = document.getElementById("message1")
+    message1.style.color = "red"
+    message1.innerHTML = "vännen"
+
+}
